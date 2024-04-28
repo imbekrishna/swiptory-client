ce70fcddac3d58a66586e80888f9d47b56d04bfe
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-04-28 14:41:46 +0530

    added category sections on homepage

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,17 +1,65 @@
 import Story from "@components/Story";
 import styles from "./styles.module.css";
+import api from "@/api/api_instance";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-const Section = () => {
-  const data = Array.from({ length: 4 }, (i) => i);
+const Section = ({ category }) => {
+  const [stories, setStories] = useState([]);
+
+  const firstRef = useRef(true);
+  const pageRef = useRef({ currentPage: null, totalPages: null });
+
+  const getStories = useCallback(
+    async (page = 1) => {
+      try {
+        const res = await api.get(
+          `/api/story?category=${category._id}&page=${page}`
+        );
+        const { data, currentPage, totalPages } = res.data;
+        setStories((prev) => [...prev, ...data]);
+        pageRef.current.currentPage = currentPage;
+        pageRef.current.totalPages = totalPages;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [category._id]
+  );
+
+  useEffect(() => {
+    if (firstRef.current === true) {
+      getStories();
+      firstRef.current = false;
+    }
+  }, [getStories]);
+
+  const fetchNextPage = () => {
+    const { currentPage, totalPages } = pageRef.current;
+    if (currentPage == totalPages) {
+      return;
+    }
+    getStories(currentPage + 1);
+  };
 
   return (
     <div className={styles.wrapper}>
-      <h2>Top stories about food</h2>
-      <div className={styles.storiesWrapper}>
-        {data.map((i) => (
-          <Story key={i} />
-        ))}
-      </div>
+      <h2>Top stories about {category.name}</h2>
+      {stories.length === 0 ? (
+        <div className={styles.noStories}>No stories available</div>
+      ) : (
+        <>
+          <div className={styles.storiesWrapper}>
+            {stories.map((story) => (
+              <Story key={story._id} story={story} />
+            ))}
+          </div>
+          {pageRef.current.currentPage !== pageRef.current.totalPages && (
+            <button className="bgPrimary textLight" onClick={fetchNextPage}>
+              See all
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Section from "@/components/Section";
 import Categories from "@components/Categories";
+import { CategoryContext } from "@/contexts/CategoryContext";
+import { useContext } from "react";
 
 const Home = () => {
+  const { categories } = useContext(CategoryContext);
+
   return (
     <div style={{ fontSize: "2rem" }}>
       <Categories />
-      <Section />
+      {categories?.map((category) => (
+        <Section key={category._id} category={category} />
+      ))}
     </div>
   );
 };

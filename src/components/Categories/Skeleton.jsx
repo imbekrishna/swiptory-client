093fbe26267347367317ef649e946c3e312609f3
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-04-29 20:16:35 +0530

    minor ui fixes

--- src/components/Categories/Skeleton.jsx
+++ src/components/Categories/Skeleton.jsx
@@ -2,14 +2,6 @@ import clsx from "clsx";
 import styles from "./styles.module.css";
 
 const Skeleton = () => {
-  return (
-    <div
-      className={clsx(styles.category, styles.shimmer)}
-      style={{
-        background:
-          "linear-gradient(rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.1) 50% , rgba(0, 0, 0, 0.4) 100%)",
-      }}
-    ></div>
-  );
+  return <div className={clsx(styles.category, styles.shimmer)}></div>;
 };
 export default Skeleton;
